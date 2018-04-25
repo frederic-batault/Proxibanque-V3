baf252fd7583801e740dabc6986af120d9ad73df
frederic.batault <frederic.batault@hotmail.fr>
2018-04-25 18:53:30 +0200

    travail sur l'affichage de la liste des clients

--- Workspace/ProxibanqueV3/src/main/java/fr/gtm/presentation/LoginBean.java
+++ Workspace/ProxibanqueV3/src/main/java/fr/gtm/presentation/LoginBean.java
@@ -1,18 +1,24 @@
 package fr.gtm.presentation;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 
+import fr.gtm.domaine.ClientDomaine;
+import fr.gtm.domaine.Conseiller;
+import fr.gtm.service.ClientService;
 import fr.gtm.service.ConseillerService;
 
-@ManagedBean(name = "loginbean")
+@ManagedBean(name = "loginBean")
 public class LoginBean implements Serializable {
 
 	// propriétés
 	private String login;
 	private String motDePasse;
 	private static final long serialVersionUID = 1L;
+	private List<ClientDomaine> listeClients;
+	private Conseiller refConseiller;
 
 	// méthodes get/set
 	public String getLogin() {
@@ -31,6 +37,22 @@ public class LoginBean implements Serializable {
 		this.motDePasse = motDePasse;
 	}
 
+	public List<ClientDomaine> getListeClients() {
+		return listeClients;
+	}
+
+	public void setListeClients(List<ClientDomaine> listeClients) {
+		this.listeClients = listeClients;
+	}
+
+	public Conseiller getRefConseiller() {
+		return refConseiller;
+	}
+
+	public void setRefConseiller(Conseiller refConseiller) {
+		this.refConseiller = refConseiller;
+	}
+
 	// constructeurs
 	public LoginBean(String login, String motDePasse) {
 		super();
@@ -42,7 +64,7 @@ public class LoginBean implements Serializable {
 		super();
 	}
 
-	// Méthode de taitement
+	// Méthodes de taitement
 	public Object authentification() {
 		// instanciation de la classe service
 		ConseillerService refConseillerService = new ConseillerService();
@@ -50,11 +72,19 @@ public class LoginBean implements Serializable {
 		boolean authentifie = refConseillerService.authentification(login, motDePasse);
 		// envoi à la bonne page en fonction du résultat
 		if (authentifie == true) {
-
+			this.refConseiller = refConseillerService.getConseiller(login);
 			return "Conseiller";
 
 		} else {
 			return "ErreurConnexion";
 		}
+	}
+
+	public Object lister() {
+		ClientService refClientService = new ClientService();
+		ConseillerService refConseillerService = new ConseillerService();
+		this.listeClients = refClientService.getAllClient(this.refConseiller);
+		return "Conseiller";
+
 	}
 }
